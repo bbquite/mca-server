41e3a0ecc3a9979596d17190a62535e66a76ba62
Aleksey Fomichev <alfom921@yandex.ru>
2024-04-30 10:12:17 +0300

    add serv iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	// http.HandleFunc(`/api`, apiPage)
+	// http.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
